161d7d26cfcdf9e29e4504b4f6bc3743cd876d55
Caio-Belom <caiobelommack@gmail.com>
2025-08-14 23:28:30 +0000

    Primeiro programa em C

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    //saida de dados em C
+    printf("meu primeiro programa em C!\n");
+    printf("Agora estou pronto para os desafio em C!\n");
+
+    return 0;
+}
